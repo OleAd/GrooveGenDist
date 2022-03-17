536a4544cbf3c53d6466e1c2ea1bd8676ef52f4e
Ole <31667950+OleAd@users.noreply.github.com>
2022-03-17 20:34:43 +0100

    Some primitive tests

--- tests/tests.py
+++ tests/tests.py
@@ -7,3 +7,38 @@ Created on Fri Feb  4 15:10:07 2022
 
 @author: olehe
 """
+import unittest
+import shutil
+import tempfile
+import groovegenerator as gg
+import os
+
+
+
+class TestSum(unittest.TestCase):
+	
+
+	def test_generate_random_pattern(self):
+		randPattern = gg.generateRandomPattern()
+		self.assertEqual(randPattern.shape, (3,32), 'Wrong shape of random pattern')
+		
+	def test_generate_constrained_pattern(self):
+		constrainedPattern = gg.generateConstrainedPattern()
+		self.assertEqual(constrainedPattern.shape, (3,32), 'Wrong shape of constrained pattern')
+	
+	def test_load_save_pattern(self):
+		test_dir = tempfile.mkdtemp()
+		savePath = test_dir
+		self.saveName = savePath + '\\testPattern.csv'
+		saved = gg.savePattern(gg.generateRandomPattern(), self.saveName, verbose=False)
+		self.assertEqual(os.path.isfile(self.saveName), True, 'Pattern csv not saved succesfully')
+
+		loaded = gg.loadPattern(self.saveName)
+		self.assertEqual(loaded.shape, (3,32), 'Loading pattern from csv failed')
+		
+		shutil.rmtree(test_dir)
+		
+
+if __name__ == '__main__':
+	unittest.main()
+		
